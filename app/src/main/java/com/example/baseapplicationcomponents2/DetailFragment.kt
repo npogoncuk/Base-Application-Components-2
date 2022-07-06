bfa78cab6f7dfe35c4cd316e4579fa1376f176b2
npogoncuk <npogoncuk@gmail.com>
2022-07-06 23:00:11 +0300

    Added navigation

--- app/src/main/java/com/example/baseapplicationcomponents2/DetailFragment.kt
+++ app/src/main/java/com/example/baseapplicationcomponents2/DetailFragment.kt
@@ -5,20 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.baseapplicationcomponents2.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var binding: FragmentDetailBinding? = null
+    private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(args) {
+            binding?.name?.text = name
+            binding?.number?.text = number
+            binding?.organization?.text = organization
+            binding?.email?.text = email
+        }
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
--- app/src/main/java/com/example/baseapplicationcomponents2/MyContactRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/baseapplicationcomponents2/MyContactRecyclerViewAdapter.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.navigation.findNavController
 
 import com.example.baseapplicationcomponents2.databinding.FragmentItemBinding
 import com.example.baseapplicationcomponents2.model.Contact
@@ -16,7 +18,6 @@ class MyContactRecyclerViewAdapter(
 ) : RecyclerView.Adapter<MyContactRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
         return ViewHolder(
             FragmentItemBinding.inflate(
                 LayoutInflater.from(parent.context),
@@ -24,7 +25,6 @@ class MyContactRecyclerViewAdapter(
                 false
             )
         )
-
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -35,9 +35,21 @@ class MyContactRecyclerViewAdapter(
 
     override fun getItemCount(): Int = contacts.size
 
-    inner class ViewHolder(binding: FragmentItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(binding: FragmentItemBinding) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
+        init {
+            binding.root.setOnClickListener(this)
+        }
         val textViewName: TextView = binding.itemName
         val textViewNumber: TextView = binding.itemNumber
+
+        override fun onClick(view: View) {
+            val contact = contacts[layoutPosition]
+            with(contact) {
+                val action = ContactFragmentDirections.actionContactFragmentToDetailFragment(name, number, organization, email)
+                view.findNavController().navigate(action)
+            }
+
+        }
 
     }
 
